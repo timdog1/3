ddd6f496397573fbf10e70742071327e93e4c599
timdog1 <99424068+timdog1@users.noreply.github.com>
2022-03-22 02:37:21 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Home from 'pages/Home/Home'
-// import context variables
-// import { useNavigation } from 'hook/Context/NavigationContext'
 
 const Routes = () => {
-  // context variables
-  // const { navigation } = useNavigation()
+
   return (
     <Router>
       <Switch>
