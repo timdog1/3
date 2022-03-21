73828fad150903b97bae192a6c605877c3bd6baa
timdog1 <99424068+timdog1@users.noreply.github.com>
2022-03-22 00:11:20 +0300

    Update TeamMeetSection.js

--- src/pages/Home/TeamMeetSection.js
+++ src/pages/Home/TeamMeetSection.js
@@ -35,8 +35,9 @@ const TeamMeetSection = () => {
                 <Grid item xs={12} sm={12} md={7} lg={7} style={{ alignItems: 'center' }}>
                   <Box>
                     <Typography className={globalClasses.text21}>
-                    İsmail Can DOĞAN (CO-Founder-Designer-Community Manager-Developer)<a
+                    İsmail Can DOĞAN (CO-Founder-Designer-Community Manager-Developer)
 
+                    -
 
                     Umut AĞGÜL (CO-Founder-Developer-Community Manager-NFT Activist)
                     </Typography>
